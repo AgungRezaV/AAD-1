34e4949867cd22572846d44d6f24c335411db806
AgungRezaV <agung.rezavergiawan.tik20@mhsw.pnj.ac.id>
2023-07-10 13:37:31 +0700

    [Update] Re-submission Fixed Unit Test.

--- app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -4,9 +4,14 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.init
+import androidx.test.espresso.intent.Intents.release
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
 import org.junit.Before
 import org.junit.Test
 
@@ -19,8 +24,11 @@ class TaskActivityTest {
 
     @Test
     fun assertActivityStatus() {
+        init()
         onView(withId(R.id.fab)).perform(ViewActions.click())
         onView(withId(R.id.add_ed_title)).check(matches(isDisplayed()))
         onView(withId(R.id.add_ed_description)).check(matches(isDisplayed()))
+        Intents.intended(hasComponent(AddTaskActivity::class.java.name))
+        release()
     }
 }
